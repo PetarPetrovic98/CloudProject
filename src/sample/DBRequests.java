4df6942e4388cf6983a6e6b8f55eb94ba21c07fa
PetarPetrovic98 <petrovic98@hotmail.com>
2021-06-10 15:58:29 +0200

    Update DBRequests.java

--- src/sample/DBRequests.java
+++ src/sample/DBRequests.java
@@ -8,12 +8,10 @@ import java.net.URL;
 
 public class DBRequests {
 
-    private static void connectToCloudBackend() throws IOException
-    {
-        System.out.println("Cloud backend");
+    private void bookRequest(String timestamp, String booked_date, String movie_id, String user_id) throws IOException {
 
         StringBuilder result = new StringBuilder();
-        URL url = new URL("https://murmuring-plateau-65295.herokuapp.com/orders?timestamp=x&booked_date=z&movie_id=y&user_id=p");
+        URL url = new URL("https://murmuring-plateau-65295.herokuapp.com/orders?timestamp=A&booked_date=B&movie_id=C&user_id=D&seats=E");
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("POST");
@@ -25,5 +23,25 @@ public class DBRequests {
         }
 
         System.out.print(result.toString());
+        
+    }
+
+    private void recivedReceiptRequest(String order_id, String movie_id, String booked_date, int people, String seat, String username) throws IOException {
+
+        StringBuilder result = new StringBuilder();
+        URL url = new URL("https://murmuring-plateau-65295.herokuapp.com/orders?order_id=A&movie_id=B&booked_date=C&people=D&seats=E&userName=F");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        try (var reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                result.append(line);
+            }
+        }
+
+        System.out.print(result.toString());
+
+        //TODO: Send the variables to the corresponding UI
     }
 }
