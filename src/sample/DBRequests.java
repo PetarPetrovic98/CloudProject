5cf456dd3a923bc408f07d36db555056328ffbe9
PetarPetrovic98 <petrovic98@hotmail.com>
2021-06-11 21:42:46 +0200

    Added new requests to endpoints

--- src/sample/DBRequests.java
+++ src/sample/DBRequests.java
@@ -8,10 +8,11 @@ import java.net.URL;
 
 public class DBRequests {
 
-    private void bookRequest(String timestamp, String booked_date, String movie_id, String user_id) throws IOException {
+    //When a client books a movie this method is called
+    private void orderRequest(String timestamp, String booked_date, String movie_id, String user_id, int seat) throws IOException {
 
         StringBuilder result = new StringBuilder();
-        URL url = new URL("https://murmuring-plateau-65295.herokuapp.com/orders?timestamp=A&booked_date=B&movie_id=C&user_id=D&seats=E");
+        URL url = new URL("https://murmuring-plateau-65295.herokuapp.com/orders?timestamp="+timestamp+"&booked_date="+booked_date+"&movie_id="+movie_id+"&user_id="+user_id+"&seats="+user_id);
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("POST");
@@ -26,10 +27,31 @@ public class DBRequests {
 
     }
 
-    private void recivedReceiptRequest(String order_id, String movie_id, String booked_date, int people, String seat, String username) throws IOException {
+    //When a client views the orders this method is called
+    private void orderRequest(int user_id) throws IOException {
 
         StringBuilder result = new StringBuilder();
-        URL url = new URL("https://murmuring-plateau-65295.herokuapp.com/orders?order_id=A&movie_id=B&booked_date=C&people=D&seats=E&userName=F");
+        URL url = new URL("https://murmuring-plateau-65295.herokuapp.com/ORDERS?user_id="+user_id);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        try (var reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                result.append(line);
+            }
+        }
+
+        System.out.print(result.toString());
+
+        //TODO: Send the variables to the corresponding UI
+    }
+
+    //To be able to see what seats are booked/free, this method is called
+    private void returnSeatRequest(int movie_id, String booked_date) throws IOException {
+
+        StringBuilder result = new StringBuilder();
+        URL url = new URL("https://murmuring-plateau-65295.herokuapp.com/occupiedSeats?movie_id="+movie_id+"&booked_date="+booked_date);
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
