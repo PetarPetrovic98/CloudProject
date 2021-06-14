9eb9e78ad94b3250aa413a3000f1053ea586898f
Emil Assarsson <emil.assarsson@hotmail.se>
2021-06-14 16:18:32 +0200

    login logic

--- src/sample/LogIn.java
+++ src/sample/LogIn.java
@@ -8,7 +8,12 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LogIn {
 
@@ -26,25 +31,31 @@ public class LogIn {
 
 
     public void userLogIn(ActionEvent event) throws IOException {
-        checkLogin();
-
+        login();
     }
 
-    private void checkLogin() throws IOException {
+    private void login() throws IOException{
+        StringBuilder result = new StringBuilder();
+        URL url = new URL("https://murmuring-plateau-65295.herokuapp.com/login?username=" + username.getText() + "&password=" + password.getText());
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        try (var reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                result.append(line);
+                System.out.println(result);
+            }
+        }catch (IOException e){
+            if (username.getText().isEmpty() || password.getText().isEmpty())
+                wrongLogIn.setText("Please enter your data.");
+            else
+                wrongLogIn.setText("Wrong username or password!");
+
+            return;
+        }
+
         Main m = new Main();
-        if(username.getText().toString().equals("marija") && password.getText().toString().equals("12345")) {
-            wrongLogIn.setText("Success!");
-
-            m.changeScene("welcomePage.fxml");
-        }
-
-        else if(username.getText().isEmpty() && password.getText().isEmpty()) {
-            wrongLogIn.setText("Please enter your data.");
-        }
-
-
-        else {
-            wrongLogIn.setText("Wrong username or password!");
-        }
+        m.changeScene("welcomePage.fxml");
     }
 }
