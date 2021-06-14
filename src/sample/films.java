826ae8ca83743ac234af2683bf9d8d92e0d00d4f
Marija Rankovic <maievmau@gmail.com>
2021-06-14 17:18:30 +0200

    Added UI for all the movies, not just Cruella.Added some new images. Added info. They all properly link to everything. Reduced the idea to just 1 controller cruella. Will change the name later.

--- src/sample/films.java
+++ src/sample/films.java
@@ -26,4 +26,24 @@ public class films {
         Main m = new Main();
         m.changeScene("cruella.fxml");
     }
+
+    public void viewKong(MouseEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("kong.fxml");
+    }
+
+    public void viewSpiral(MouseEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("spiral.fxml");
+    }
+
+    public void vievNobody(MouseEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("nobody.fxml");
+    }
+
+    public void viewTove(MouseEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("tove.fxml");
+    }
 }
